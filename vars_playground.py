ac85f50b4d7f5d108b9e3a6e274cb62518063de5
urgentlion <urgentlioninfo@gmail.com>
2022-12-17 11:34:17 -0800

    ...

--- vars_playground.py
+++ vars_playground.py
@@ -1,2 +1,15 @@
 print("This is a test")
 print("this is a second test to show that github is configured with local repository")
+#This is a single-line line comment for testing.
+
+"""
+This is a 
+multi-line comment
+appearing in multiple lines.
+"""
+
+'''
+This is another
+multi-line comment
+appearing in multiple lines.
+'''
