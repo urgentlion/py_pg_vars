24025e7a50fcce757b9b5aac1a95369fa5aa3ec4
urgentlion <urgentlioninfo@gmail.com>
2022-12-21 18:26:11 -0800

    ...

---
+++ inputfn_playground.py
@@ -0,0 +1,14 @@
+team = input("What is your favorite sports team? ")
+food_ = input("What is your favorite food? ")
+hobbies = input("What are some of your hobbies? ")
+cooking1 = input("Do you like to cook? ")
+cooking2 = input("What are some of your favorite things to cook? ")
+
+print(team)
+print(food_, "is really good")
+print(hobbies, "is interesting")
+print(cooking1 + "?", "Wow!")
+print(cooking2, "is really hard for me to cook")
+
+name = input("What is your name? ")
+print("Hello", name)
